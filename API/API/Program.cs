57b5d5dd4a40f871baa5db5e85f983f3eff0b318
Auto Logon <autologon@CSED.LAB>
2024-03-22 11:38:52 -0300

    criando Lista de produtos

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -1,13 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+List<Produto> produtos = new List<Produto>();
+produtos.Add(new Produto("Celular","Android"));
+produtos.Add(new Produto("Celular","Android"));
+produtos.Add(new Produto("Celular","Android"));
+produtos.Add(new Produto("Celular","Android"));
+
 //GET - http://localhost:5189/
 app.MapGet("/", () => "Minha primeira API em C#");
 
 //GET - http://localhost:5189/api/produto/listar
-app.MapGet("/api/produto/listar", () => "Listagem de Produtos");
+app.MapGet("/api/produto/listar", () => produtos);
 
 //POST - http://localhost:5189/api/produto/cadastrar
 app.MapPost("/api/produto/cadastrar", () => "Cadastrando os Produtos");
 
 app.Run();
+
+record Produto(string Nome, string Descricao);
