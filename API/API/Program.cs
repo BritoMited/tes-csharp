00bd3564bbb56fd7b4be93436864c62a000c1ee6
Auto Logon <autologon@CSED.LAB>
2024-03-22 11:22:16 -0300

    GET POST

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -1,6 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+//GET - http://localhost:5189/
 app.MapGet("/", () => "Minha primeira API em C#");
+
+//GET - http://localhost:5189/api/produto/listar
+app.MapGet("/api/produto/listar", () => "Listagem de Produtos");
+
+//POST - http://localhost:5189/api/produto/cadastrar
+app.MapPost("/api/produto/cadastrar", () => "Cadastrando os Produtos");
 
 app.Run();
