5028dd0ca3cd1a4c6d0667c6f6960702445eb7be
Brito <gustavo.ponciano2005@outlook.com>
2024-03-24 23:58:38 -0300

    Post funcionando

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -1,5 +1,7 @@
+using System.Text.Json;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
 
 List<Produto> produtos = new List<Produto>();
 produtos.Add(new Produto("Celular","Android"));
@@ -14,7 +16,15 @@ app.MapGet("/", () => "Minha primeira API em C#");
 app.MapGet("/api/produto/listar", () => produtos);
 
 //POST - http://localhost:5189/api/produto/cadastrar
-app.MapPost("/api/produto/cadastrar", () => "Cadastrando os Produtos");
+app.MapPost("/api/produto/lista", (Produto produto) =>
+{
+    produtos.Add(produto);
+
+    return "Produto cadastrado com sucesso!";
+});
+
+//POST - http://localhost:5189/api/produto/cadastrar
+app.MapPost("/api/produto/cadastrar", () => "Produto cadastrado com sucesso!");
 
 app.Run();
 
